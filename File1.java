4391297b2af45fcf64a4585dc664c236f67dc198
Richard Otte <rotte@blackducksoftware.com>
2021-02-22 12:03:33 -0500

    Update File1.java

--- File1.java
+++ File1.java
@@ -1,6 +1,11 @@
 public class File1 {
   public static void main(String[] args) {
     String foo = "bump";
+    int iDump = 0;
+    for (int i = 0; i>-1; i++) {
+      iDump = i;
+    }
+    System.out.println("" + iDump);
   }
   
 }
